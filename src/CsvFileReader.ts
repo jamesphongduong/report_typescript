59293fa67c51a0812d4b73774cd7c6cb7b921517
James Duong <james@simble.io>
2019-11-12 10:55:56 +1100

    116

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,10 +1,11 @@
 import fs from "fs";
-import { dateStringToDate } from "./utils";
 
-export class CsvFileReader {
-  data: string[][] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -15,8 +16,6 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(",");
       })
-      .map((row: string[]): any => {
-        return [dateStringToDate(row[0])];
-      });
+      .map(this.mapRow);
   }
 }
